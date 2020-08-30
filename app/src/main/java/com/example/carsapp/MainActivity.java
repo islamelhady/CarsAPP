6f19319e466c7afe5c5e8feaffd336d3d1aadd83
islamelhady <i.elhady44@gmail.com>
2020-08-30 02:13:52 +0200

    design project

--- app/src/main/java/com/example/carsapp/MainActivity.java
+++ app/src/main/java/com/example/carsapp/MainActivity.java
@@ -22,29 +22,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        recyclerView = findViewById(R.id.rv_main);
-
-
-//        database = new MyDatabase(this);
-
-        DatabaseAccess databaseAccess =  DatabaseAccess.getInstance(this);
-        ArrayList<Car> cars = new ArrayList<>();
-        databaseAccess.open();
-        cars = databaseAccess.getAllCars();
-        databaseAccess.close();
-
-
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(cars);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this,2);
-
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-
-
-
+        setContentView(R.layout.activity_view_car);
+//
+//        recyclerView = findViewById(R.id.rv_main);
+//
+//
+////        database = new MyDatabase(this);
+//
+//        DatabaseAccess databaseAccess =  DatabaseAccess.getInstance(this);
+//        ArrayList<Car> cars = new ArrayList<>();
+//        databaseAccess.open();
+//        cars = databaseAccess.getAllCars();
+//        databaseAccess.close();
+//
+//
+//        RecyclerViewAdapter adapter = new RecyclerViewAdapter(cars);
+//        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this,2);
+//
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(layoutManager);
+//        recyclerView.setAdapter(adapter);
+//
+//
+//
     }
 
 
